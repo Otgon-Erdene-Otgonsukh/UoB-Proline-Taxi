3c11efbad71fe9d88bda45507371e3cf9bc253b9
Ioan Moir <ok24616@bristol.ac.uk>
2025-10-17 18:55:11 +0100

    Added FAQ page and Dropdown Info Box component
    FAQ Page:
    Sections on basic FAQs (to be updated) and a Contact section with contact details for Proline.
    
    Dropdown Info Box component:
    Requires onClick callback, returns a button component which can be clicked in order to show more information (description) about the title (question/title).

---
+++ component/Dropdown_info_box.tsx
@@ -0,0 +1,23 @@
+// Defines dropdown/info component for FAQ-style title/descriptions.
+export default function DdInfoBox({title, description, open, click,}: {title: string; description: string; open: boolean; click: () => void;}) {
+  return (
+    // ${open ? ...} makes the background white (slightly easier to read thinner font) if the element is open.
+    <div className={`my-4 border border-black rounded-md ${open ? "bg-white" : ""}`}>
+        {/* onClick passed as callback because otherwise it would not update the state of the page. */}
+        <button onClick={click}
+            className="p-4 cursor-pointer w-full flex justify-between items-center text-left font-bold text-gray-800 focus:outline-none"
+            aria-expanded={open}>
+              
+            {title}
+            {/* Turn the arrow upside down if the dropdown is opened. */}
+            {/* NOTE: I used the ascii character for an upside down triangle here, we should replace this with a real icon element later. */}
+            <p className={`h-5 w-5 transition-transform duration-100 ${open ? "rotate-180" : ""}`}>▼</p>
+        </button>
+
+        {/* Opened and closed animation / states are determined by the height and opacity judging by the state open == true / false. */}
+        <div className={`transition-all duration-100 ease-in-out overflow-hidden ${open ? "px-4 pb-4 max-h-40 opacity-100 mt-2" : "max-h-0 opacity-0"}`}>
+            <p className="text-gray-600 leading-relaxed">{description}</p>
+        </div>
+    </div>
+  );
+}
