2c4b2f7031b5fe1e9601a5c187e50229226fd5a1
Yidi <152907002+Dehors777@users.noreply.github.com>
2025-10-25 19:29:39 +0800

    Add the foreign key dependent(Booking references Trip)

--- db/01.sql
+++ db/01.sql
@@ -77,3 +77,9 @@ CREATE TABLE Booking (
     booking_status INT DEFAULT 0 CHECK (booking_status IN (0, 1, 2)), -- e.g. 0=awaiting, 1=approved, 2=rejected
     time_created BIGINT DEFAULT EXTRACT(EPOCH FROM NOW())::BIGINT
 );
+
+-- Change the foreign key dependent (Booking references Trip)
+ALTER TABLE Booking
+ADD CONSTRAINT fk_booking_trip
+FOREIGN KEY (trip_id) REFERENCES Trip(trip_id) ON DELETE SET NULL;
+
