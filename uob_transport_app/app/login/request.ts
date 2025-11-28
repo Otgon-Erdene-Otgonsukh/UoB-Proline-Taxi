dec7ceead45988f10fc2d3b90cd0897c658de3d5
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2025-11-28 15:16:08 +0000

    definitely not going to count as a valid commit but no choice, moved the codebase to a dedicated folder

---
+++ uob_transport_app/app/login/request.ts
@@ -0,0 +1,15 @@
+import { easyPostRequest } from "@/utils/easyRequest";
+
+export const userLogin = async (
+  email: string,
+  password: string
+): Promise<Response> => {
+  return easyPostRequest(
+    "login",
+    {
+      email,
+      password,
+    },
+    false
+  );
+};
