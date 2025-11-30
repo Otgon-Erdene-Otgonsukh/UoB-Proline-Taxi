487f8f76e5a24acc1877d224d50f32cc67405c9b
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2025-11-30 01:41:18 +0000

    updated booking page to send the flight number and airport details in the api request

--- uob_transport_app/app/book/page.tsx
+++ uob_transport_app/app/book/page.tsx
@@ -107,7 +107,7 @@ export default function BookingPage() {
       }
 
       loc = formData.CustomLoc;
-    } else {
+    } else if (!isFlightChecked){
       // Common Pickup Location / Dropdown
       if (formData.CommonLoc == "") {
         addFormFeedback("CommonLoc", "Please pick one.");
@@ -431,7 +431,7 @@ export default function BookingPage() {
                   <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                 </label>
               </div>
-              {isViaChecked && !isManualChecked && (
+              {isViaChecked && !isManualChecked && !isFlightChecked && (
                 <div className="flex flex-col">
                   <label htmlFor="via" className="mb-1 text-sm">
                     Via
@@ -539,6 +539,21 @@ export default function BookingPage() {
                       {formFeedback.Airport}
                     </FormHelperText>
                   </div>
+                  {isViaChecked && (
+                    <div className="flex flex-col">
+                      <label htmlFor="via" className="mb-1 text-sm">
+                        Via
+                      </label>
+                      <input
+                        id="via"
+                        placeholder="Via..."
+                        className="border-2 rounded px-3 py-2"
+                        onChange={(e) => {
+                          setFormData({ ...formData, Via: e.target.value });
+                        }}
+                      ></input>
+                    </div>
+                  )}
                 </div>
               )}
 
@@ -588,9 +603,7 @@ export default function BookingPage() {
                     <input
                       id="returnPickUp"
                       className="border-2 rounded px-3 py-2"
-                      value={
-                        formData.DropoffLoc
-                      }
+                      value={formData.DropoffLoc}
                       disabled
                     ></input>
                   </div>
