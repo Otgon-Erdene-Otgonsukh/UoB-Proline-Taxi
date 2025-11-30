218e4dfe6fc9a64b0c268f91a1c5b29de7fa320c
Yidi <152907002+Dehors777@users.noreply.github.com>
2025-12-01 00:22:05 +0800

    create user reset when user want to send reset email

--- uob_transport_app/app/api/reset-email/route.ts
+++ uob_transport_app/app/api/reset-email/route.ts
@@ -1,3 +1,5 @@
+import { createUserResetAccess } from "@/backend/access/user_reset_access";
+import { generateUuid } from "@/backend/utils/uuid";
 import { NextRequest } from "next/server";
 import { createTransport } from "nodemailer"
 
@@ -16,6 +18,14 @@ export async function POST(request: NextRequest) {
   const requestJson = await request.json()
   const toEmail = requestJson['email']
 
+  const uuid = generateUuid()
+  console.log(toEmail);
+  console.log(uuid);
+
+  // TODO Check if there already has a field in the UserReset Table
+  const userReset = await createUserResetAccess(toEmail, uuid)
+  console.log(userReset);
+
   const transporter = createTransport({
     host: 'smtp.gmail.com',
     port: 587,
@@ -25,23 +35,30 @@ export async function POST(request: NextRequest) {
     }
   })
 
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USERNAME,
-      to: toEmail,
-      subject: 'Reset Password',
-      text: 'Please go to the following link to reset the password:\n',
-      replyTo: process.env.EMAIL_USERNAME,
-    })
-    return new Response(JSON.stringify({
-      message: 'send success'
-    }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    console.log(error);
-
+  if (userReset) {
+    try {
+      await transporter.sendMail({
+        from: process.env.EMAIL_USERNAME,
+        to: toEmail,
+        subject: 'Reset Password',
+        text: `Please go to the following link to reset the password: \nhttp://localhost:3000/home/reset-password?uuid=${userReset.uuid} \nThe link expired at ${userReset.expired_at}`,
+        replyTo: process.env.EMAIL_USERNAME,
+      })
+      return new Response(JSON.stringify({
+        message: 'send success'
+      }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } catch (error) {
+      return new Response(JSON.stringify({
+        message: 'send email failed, try again later'
+      }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  } else {
     return new Response(JSON.stringify({
       message: 'send email failed, try again later'
     }), {
@@ -49,10 +66,5 @@ export async function POST(request: NextRequest) {
       headers: { 'Content-Type': 'application/json' },
     });
   }
-
-
-
-
-
 
 }
--- uob_transport_app/backend/access/user_reset_access.ts
+++ uob_transport_app/backend/access/user_reset_access.ts
@@ -1,14 +1,13 @@
-import { PrismaClient } from '@/generated/prisma/client'
-import { user_reset } from "@/generated/prisma/client"
+import { PrismaClient, user_reset } from '@/generated/prisma/client'
 
 const prisma = new PrismaClient()
 
-export const createUserReset = async (userId: number, uuid: string): Promise<user_reset | null> => {
+export const createUserResetAccess = async (email: string, uuid: string): Promise<user_reset | null> => {
   const tomorrow = new Date()
   tomorrow.setDate(tomorrow.getDate() + 1)
   return prisma.user_reset.create({
     data: {
-      user_id: userId,
+      email,
       uuid,
       expired_at: tomorrow
     },
