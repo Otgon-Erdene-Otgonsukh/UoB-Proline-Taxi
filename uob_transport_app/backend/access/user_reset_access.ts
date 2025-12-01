80cd9016f286f4819379035d03c48e47ed88c403
Yidi <152907002+Dehors777@users.noreply.github.com>
2025-12-01 11:05:28 +0800

    change the findFirst to findUnique since email is unique

--- uob_transport_app/backend/access/user_reset_access.ts
+++ uob_transport_app/backend/access/user_reset_access.ts
@@ -15,7 +15,7 @@ export const createUserResetAccess = async (email: string, uuid: string): Promis
 }
 
 export const getUserResetAccess = async (email: string): Promise<user_reset | null> => {
-  return prisma.user_reset.findFirst({
+  return prisma.user_reset.findUnique({
     where: {
       email,
     },
