f8d877401a8bff1ea3944cd5d5d997fe5c60399a
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2025-12-08 00:18:03 +0000

    protected the book page and sends the id of the currently logged in user to link the booking

--- uob_transport_app/app/book/page.tsx
+++ uob_transport_app/app/book/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, redirect } from "next/navigation";
 import {
   Button,
   createTheme,
@@ -15,6 +15,7 @@ import {
   FormControlLabel,
 } from "@mui/material";
 import NumberField from "@/components/NumberField";
+import { useSession } from "next-auth/react";
 
 export default function BookingPage() {
   const commonLocations = [
@@ -26,10 +27,18 @@ export default function BookingPage() {
     "Physics Building",
   ];
 
+  const session = useSession();
+
+  if (!session) {
+    // protected page
+    redirect("/login");
+  }
+
   const [isManualChecked, setIsManualChecked] = useState(false);
   const [isFlightChecked, setIsFlightChecked] = useState(false);
   const [isViaChecked, setIsViaChecked] = useState(false);
   const [isReturnChecked, setIsReturnChecked] = useState(false);
+  const [phoneCode, setPhoneCode] = useState("+44");
 
   // Set error messages visible next to fields, default "" (empty) for hide.
   const [formFeedback, setFormFeedback] = useState({
@@ -235,14 +244,14 @@ export default function BookingPage() {
     // fail == false if all validation succeeds, then post the request.
     if (fail == false) {
       const jsonBody = {
-        user_id: 1,
+        user_id: session.data?.user.user_id,
         pickup_location: loc,
         dropoff_location: formData.DropoffLoc,
         pickup_time: pickupDateTime,
         first_name: formData.FirstName,
         surname: formData.Surname,
         email: formData.Email,
-        tel_number: formData.Number,
+        tel_number: phoneCode + " " + formData.Number,
         additional_info: formData.AdditionalInfo,
         via: formData.Via,
         returnTo: formData.ReturnTo,
@@ -396,7 +405,7 @@ export default function BookingPage() {
                     onChange={(e) => setIsManualChecked(e.target.checked)}
                     className="sr-only peer"
                   />
-                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                 </label>
                 <label
                   htmlFor="flight"
@@ -412,7 +421,7 @@ export default function BookingPage() {
                     onChange={(e) => setIsFlightChecked(e.target.checked)}
                     className="sr-only peer"
                   />
-                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                 </label>
                 <label
                   htmlFor="via"
@@ -426,7 +435,7 @@ export default function BookingPage() {
                     onChange={(e) => setIsViaChecked(e.target.checked)}
                     className="sr-only peer"
                   />
-                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 peer-checked:bg-[#4a4a4a] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                 </label>
               </div>
               {isViaChecked && !isManualChecked && (
@@ -586,9 +595,7 @@ export default function BookingPage() {
                     <input
                       id="returnPickUp"
                       className="border-2 rounded px-3 py-2"
-                      value={
-                        formData.DropoffLoc
-                      }
+                      value={formData.DropoffLoc}
                       disabled
                     ></input>
                   </div>
@@ -696,7 +703,12 @@ export default function BookingPage() {
                   Phone number
                 </label>
                 <div className="flex gap-2">
-                  <select className="border-2 rounded px-2 py-2">
+                  <select
+                    className="border-2 rounded px-2 py-2"
+                    onChange={(e) => {
+                      setPhoneCode(e.target.value);
+                    }}
+                  >
                     <option value="+44">+44 (UK)</option>
                     <option value="+1">+1 (US/CA)</option>
                     <option value="+91">+91 (IN)</option>
@@ -783,7 +795,7 @@ export default function BookingPage() {
                 </label>
                 <textarea
                   id="addInfo"
-                  className={`border-2 rounded px-3 py-2 min-h-[80px] ${
+                  className={`border-2 rounded px-3 py-2 min-h-20 ${
                     formFeedback.AdditionalInfo == "" ? "" : "border-red-700"
                   }`}
                   onChange={(e) => {
