42acb480ec473ccef80a85c699c9c9ec02c5af99
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2026-01-07 21:06:18 +0000

    added the return pick up time column to the trip table and updated schema and updated the create-booking api to include the added field

--- uob_transport_app/model/models.ts
+++ uob_transport_app/model/models.ts
@@ -16,6 +16,7 @@ export type Trip = {
   dropoff_latitude: number;
   dropoff_longitude: number;
   pickup_time?: string;
+  return_pickup_time: string,
   passenger_num: number;
   via: string;
   return_drop_loc: string;
