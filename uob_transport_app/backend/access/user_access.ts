cb79716984507321aadffc0eb5cf06c2d4af1a46
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-01-09 12:19:23 +0800

    fix user manage table filter bug

--- uob_transport_app/backend/access/user_access.ts
+++ uob_transport_app/backend/access/user_access.ts
@@ -60,15 +60,15 @@ export const getUserByEmailAccess = async (email: string): Promise<User | null> 
 
 export const getUserListAccess = async (page: number, pageSize: number, name?: string, role?: string, userStatus?: number): Promise<User[] | null> => {
   const query: { [key: string]: string | number | object } = {}
-  if (name) {
+  if (name !== undefined) {
     query['name'] = {
       contains: name
     }
   }
-  if (role) {
+  if (role !== undefined) {
     query['role'] = role
   }
-  if (userStatus) {
+  if (userStatus !== undefined) {
     query['user_status'] = userStatus
   }
   return prisma.user.findMany({
