24ed3bfd6bafd8f99e4f320c4d2a304764a1c281
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2026-01-11 20:07:34 +0000

    create profile app router and start

---
+++ uob_transport_app/app/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Avatar } from "@mui/material";
+
+export default function Profile() {
+    <div className="min-h-screen flex justify-center items-center">
+        <div>
+            <Avatar sx={{bgcolor: "#2c2c2c"}}>K</Avatar>
+        </div>
+    </div>
+}
