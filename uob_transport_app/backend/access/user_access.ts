2b2e4d83027bad2e69e22482560f93140d8e99ed
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2026-01-14 02:08:43 +0000

    updated the searchUserAccess to include the department object and updated the session interface and token to include necessary info about the user

--- uob_transport_app/backend/access/user_access.ts
+++ uob_transport_app/backend/access/user_access.ts
@@ -1,49 +1,40 @@
-import { PrismaClient, User } from '@/generated/prisma/client'
+import { PrismaClient, User, department } from "@/generated/prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export const searchUserAccess = async (email: string): Promise<User | null> => {
+export const searchUserAccess = async (
+  email: string
+): Promise<(User & { department: department | null }) | null> => {
   return prisma.user.findUnique({
     where: {
-      email
-    }
-  })
-}
+      email,
+    },
+    include: {
+      department: true,
+    },
+  });
+};
 
-export const updateUserTokenAccess = async (email: string, token: string): Promise<User | null> => {
+export const updateUserPassowrdAccess = async (
+  email: string,
+  password: string
+): Promise<User | null> => {
   return prisma.user.update({
     where: {
-      email
+      email,
     },
     data: {
-      token: token
-    }
-  })
-}
-
-export const updateUserPassowrdAccess = async (email: string, password: string): Promise<User | null> => {
-  return prisma.user.update({
-    where: {
-      email
+      password,
     },
-    data: {
-      password
-    }
-  })
-}
+  });
+};
 
-export const getUserByTokenAccess = async (token: string): Promise<User | null> => {
+export const getUserByEmailAccess = async (
+  email: string
+): Promise<User | null> => {
   return prisma.user.findUnique({
     where: {
-      token
-    }
-  })
-}
-
-export const getUserByEmailAccess = async (email: string): Promise<User | null> => {
-  return prisma.user.findUnique({
-    where: {
-      email
-    }
-  })
-}
+      email,
+    },
+  });
+};
