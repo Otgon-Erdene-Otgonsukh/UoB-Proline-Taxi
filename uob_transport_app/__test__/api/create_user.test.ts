dabb4da7e41366e0abdd71ffc2e9fe64d9021584
Otgon-Erdene-Otgonsukh <ww23805@bristol.ac.uk>
2026-02-09 22:51:30 +0000

    created a create user api end point test

---
+++ uob_transport_app/__test__/api/create_user.test.ts
@@ -0,0 +1,158 @@
+/**
+ * @jest-environment node
+ */
+import { POST } from "@/app/api/create_user/route";
+import { prismaMock } from "@/utils/singleton";
+import bcrypt from "bcryptjs";
+
+jest.mock("bcryptjs", () => ({
+  hash: jest.fn(),
+}));
+
+describe("Registration tests with ensuring behaviour", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Server side validation correctly denies invalid data", async () => {
+    const data = {
+      mail: "sdasd@gmail.com",
+      password: "something",
+      username: "dog",
+      departmentName: "Arts",
+      firstName: "Alvin",
+      lastName: "Magy",
+      role: "proline_staff",
+      phoneNumber: "+44 2134123131",
+    };
+    const body = JSON.stringify(data);
+    const req = new Request("http://localhost:3000/api/create_user", {
+      method: "POST",
+      body: body,
+    });
+    const res = await POST(req);
+    expect(res.status).toBe(500);
+  });
+
+  test("When department exists, appointment is made to the user", async () => {
+    prismaMock.department.findFirst.mockResolvedValue({
+      dep_id: 1,
+      dep_name: "Arts",
+    });
+    prismaMock.user.create.mockResolvedValue({
+      user_id: 1,
+      username: "testuser",
+    });
+    (bcrypt.hash as jest.Mock).mockResolvedValue("hashedpassword");
+
+    const data = {
+      mail: "test@gmail.com",
+      password: "something",
+      username: "testuser",
+      department: "Arts",
+      firstName: "John",
+      lastName: "Doe",
+      role: "normal_user",
+      phoneNumber: "+44 2134123131",
+    };
+
+    const body = JSON.stringify(data);
+    const req = new Request("http://localhost:3000/api/create_user", {
+      method: "POST",
+      body: body,
+    });
+
+    const res = await POST(req);
+
+    expect(res.status).toBe(200);
+    expect(prismaMock.department.create).not.toHaveBeenCalled();
+    expect(prismaMock.user.create).toHaveBeenCalledWith({
+      data: {
+        dep_id: 1,
+        username: data.username,
+        name: data.firstName,
+        surname: data.lastName,
+        phone_number: data.phoneNumber,
+        role: data.role,
+        user_status: 1,
+        email: data.mail,
+        password: "hashedpassword",
+      }
+    })
+  });
+
+  test("When a new department input comes in, new department is created and user appointed", async () => {
+    prismaMock.department.findFirst.mockResolvedValue(null);
+    prismaMock.department.create.mockResolvedValue({
+      dep_id: 3,
+    });
+    (bcrypt.hash as jest.Mock).mockResolvedValue("hashedpassword");
+
+    const data = {
+      mail: "test@gmail.com",
+      password: "something",
+      username: "testuser",
+      department: "Chemistry",
+      firstName: "John",
+      lastName: "Doe",
+      role: "normal_user",
+      phoneNumber: "+44 2134123131",
+    };
+
+    const body = JSON.stringify(data);
+    const req = new Request("http://localhost:3000/api/create_user", {
+      method: "POST",
+      body: body,
+    });
+
+    const res = await POST(req);
+    expect(res.status).toBe(200);
+    expect(prismaMock.department.create).toHaveBeenCalledWith({
+      data: {
+        dep_name: "Chemistry"
+      }
+    });
+    expect(prismaMock.user.create).toHaveBeenCalledWith({
+      data: {
+        dep_id: 3,
+        username: data.username,
+        name: data.firstName,
+        surname: data.lastName,
+        phone_number: data.phoneNumber,
+        role: data.role,
+        user_status: 1,
+        email: data.mail,
+        password: "hashedpassword",
+      }
+    })
+  });
+  
+  test("Proline staff registration does not create a new department entry", async () => {
+    (bcrypt.hash as jest.Mock).mockResolvedValue("hashedpassword");
+    prismaMock.user.create.mockResolvedValue({
+      user_id: 11
+    });
+
+    const data = {
+      mail: "test@prolinetaxi.com",
+      password: "something",
+      username: "testuser",
+      firstName: "John",
+      department: "",
+      lastName: "Doe",
+      role: "proline_staff",
+      phoneNumber: "+44 2134123131",
+    };
+
+    const body = JSON.stringify(data);
+    const req = new Request("http://localhost:3000/api/create_user", {
+      method: "POST",
+      body: body,
+    });
+
+    const res = await POST(req);
+    expect(res.status).toBe(200);
+    expect(prismaMock.user.create).toHaveBeenCalledTimes(1);
+    expect(prismaMock.department.create).not.toHaveBeenCalled();
+  })
+});
