a43e588bb931eaf262e08bbe7188f62229bf2829
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-02-15 10:33:43 +0800

    add test for departments_access

---
+++ uob_transport_app/backend/access/departments_access.ts
@@ -0,0 +1,15 @@
+import prisma from '@/utils/client'
+
+export const getDepartmentsListAccess = async (depName: string | undefined): Promise<{ dep_id: number, dep_name: string }[]> => {
+  return prisma.department.findMany({
+    select: {
+      dep_id: true,
+      dep_name: true
+    },
+    where: {
+      dep_name: {
+        contains: depName
+      }
+    }
+  })
+}
