1d947a79899808b20a5f7a6c6931bb747e655b87
Erik Maltby <maltby.e@outlook.com>
2026-03-04 11:56:20 +0000

    half way throuh, commiting now so evil git doesnt steal my stuff again

--- uob_transport_app/app/super/page.tsx
+++ uob_transport_app/app/super/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSession } from "next-auth/react";
 import { UserRecord } from "@/model/models";
 import { useTheme } from "@mui/material/styles";
@@ -43,6 +43,8 @@ import { userStatusToIntMap, userStatusToStrMap, roleStrMap, roles, roleReadable
 import { getDepartmentsList } from "./requests";
 import ConfirmDialog from "@/components/confirmDIalog";
 import { UserTable } from "@/components/SuperUsersTable";
+import CustomizedButton from "@/components/CustomizedButton";
+import { bookingStatus } from "../home/constants";
 
 interface TablePaginationActionsProps {
   count: number;
@@ -274,6 +276,32 @@ const Page = () => {
 
   const [tabValue, setTabValue] = useState(0);
 
+  const handleBookingDialogOpen = () => {
+    setEditBookDialogOpen(true);
+  };
+
+  type BookingSearchFormProps = {
+      pickUpTimeFrom?: Date;
+      pickUpTimeTo?: Date;
+      from?: string;
+      to?: string;
+      bookingStatus?: string;
+    };
+    
+    const [isSearchSubmitted, setIsSearchSubmitted] = useState(false);
+
+    const dateTimePickerFromAnchorRef = useRef<HTMLDivElement>(null);
+    const dateTimePickerToAnchorRef = useRef<HTMLDivElement>(null);
+    const [dateTimePickerFromOpen, setDateTimePickerFromOpen] = useState(false);
+    const [dateTimePickerToOpen, setDateTimePickerToOpen] = useState(false);
+
+    const [bookingSearchFormInput, setBookingSearchFormInput] = useState<BookingSearchFormProps>({
+      pickUpTimeFrom: undefined,
+      pickUpTimeTo: undefined,
+      from: undefined,
+      to: undefined,
+      bookingStatus: undefined
+    })
   return (
     <div className="flex-col font-inter">
       <header className="w-full bg-[#2c2c2c] text-white p-3 shadow-lg items-center flex gap-4 sticky top-0 z-50">
@@ -428,26 +456,378 @@ const Page = () => {
         )}
 
         {tabValue === 1 && (
-          <div>
-            <div className="flex items-center gap-2 mb-4">
-              <IconButton
-                onClick={toggleDrawer(true)}
-                sx={{
-                  color: '#2c2c2c',
-                  '&:hover': {
-                    bgcolor: '#f3f4f6',
-                  },
-                }}
+            <div>
+              <motion.div
+                className="bg-white shadow-lg/20 rounded-lg p-6 md:p-8 w-full max-w-6xl my-15 mt-13"
+                initial={{ opacity: 0, y: 6, scale: 0.98 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                transition={{ duration: 1, ease: "easeOut", delay: 0.8 }}
               >
-                <MenuIcon />
-              </IconButton>
-              <h1 className="text-xl font-aleo md:text-3xl font-semibold text-shadow-lg/20">
-                Departments
-              </h1>
-            </div>
-            <Typography sx={{ color: "gray", fontSize: 16, textAlign: "center", my: 10 }}>
-              Departments Table View (to be implemented)
-            </Typography>
+                <div className="flex justify-between mb-6">
+                  <h1 className="font-aleo text-2xl sm:text-3xl font-semibold text-shadow-lg/20">
+                    MY BOOKINGS
+                  </h1>
+                  <CustomizedButton
+                    title="+ New Booking"
+                    type="warning"
+                    click={handleBookingDialogOpen}
+                  />
+                </div>
+
+                <Box
+                  component="form"
+                  onSubmit={handleSubmitSearchForm}
+                  sx={{
+                    display: "flex",
+                    flexDirection: { xs: "column", sm: "row" },
+                    gap: 2.5,
+                    marginBottom: 3,
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    label="From"
+                    id="searchFromInput"
+                    value={bookingSearchFormInput.from}
+                    onChange={(e) => {
+                      setBookingSearchFormInput({
+                        ...bookingSearchFormInput,
+                        from: e.target.value,
+                      });
+                    }}
+                    size="small"
+                    sx={{ minWidth: 150 }}
+                  />
+                  <TextField
+                    fullWidth
+                    label="To"
+                    id="searchToInput"
+                    value={bookingSearchFormInput.to}
+                    onChange={(e) => {
+                      setBookingSearchFormInput({ ...bookingSearchFormInput, to: e.target.value });
+                    }}
+                    size="small"
+                    sx={{ minWidth: 150 }}
+                  />
+                  <FormControl sx={{ minWidth: 150 }}>
+                    <InputLabel id="searchBookingStatusInputLabel" size="small">
+                      Booking Status
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      label="Booking Status"
+                      id="searchBookingStatusInput"
+                      value={bookingSearchFormInput.bookingStatus}
+                      onChange={(e) => {
+                        setBookingSearchFormInput({
+                          ...bookingSearchFormInput,
+                          bookingStatus: e.target.value,
+                        });
+                      }}
+                      size="small"
+                    >
+                      <MenuItem value={"All"} key={"All"}>
+                        All
+                      </MenuItem>
+                      {bookingStatus.map((e) => {
+                        return (
+                          <MenuItem value={e} key={e}>
+                            {e}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    fullWidth
+                    onClick={() => setDateTimePickerFromOpen(true)}
+                    label="Pick Up Date From"
+                    size="small"
+                    sx={{ minWidth: 150 }}
+                    ref={dateTimePickerFromAnchorRef}
+                    defaultValue={bookingSearchFormInput.pickUpTimeFrom?.toDateString()}
+                    slotProps={{
+                      inputLabel: {
+                        shrink: bookingSearchFormInput.pickUpTimeFrom !== undefined,
+                      },
+                    }}
+                  />
+                  {bookingSearchFormInput.pickUpTimeFrom && (
+                    <IconButton
+                      size="small"
+                      sx={{ px: 1.3, mx: -2 }}
+                      onClick={() => {
+                        setBookingSearchFormInput({
+                          ...bookingSearchFormInput,
+                          pickUpTimeFrom: undefined,
+                        });
+                        setIsSearchSubmitted(false);
+                      }}
+                    >
+                      <CancelIcon fontSize="small" sx={{ color: "red" }} />
+                    </IconButton>
+                  )}
+                  <TextField
+                    fullWidth
+                    onClick={() => setDateTimePickerToOpen(true)}
+                    label="Pick Up Date To"
+                    size="small"
+                    sx={{ minWidth: 150 }}
+                    ref={dateTimePickerToAnchorRef}
+                    defaultValue={searchFormInput.pickUpTimeTo?.toDateString()}
+                    slotProps={{
+                      inputLabel: {
+                        shrink: searchFormInput.pickUpTimeTo !== undefined,
+                      },
+                    }}
+                  />
+                  {searchFormInput.pickUpTimeTo && (
+                    <IconButton
+                      size="small"
+                      sx={{ px: 1.3, mx: -2 }}
+                      onClick={() => {
+                        setSearchFormInput({
+                          ...searchFormInput,
+                          pickUpTimeTo: undefined,
+                        });
+                        setIsSearchSubmitted(false);
+                      }}
+                    >
+                      <CancelIcon fontSize="small" sx={{ color: "red" }} />
+                    </IconButton>
+                  )}
+                  <DateTimePicker
+                    open={dateTimePickerFromOpen}
+                    onClose={() => setDateTimePickerFromOpen(false)}
+                    anchorEl={dateTimePickerFromAnchorRef}
+                    selectedDate={searchFormInput.pickUpTimeFrom || null}
+                    onDateChange={(date) => {
+                      setSearchFormInput({ ...searchFormInput, pickUpTimeFrom: date });
+                      setIsSearchSubmitted(false);
+                    }}
+                    locale={enLocale}
+                  />
+                  <DateTimePicker
+                    open={dateTimePickerToOpen}
+                    onClose={() => setDateTimePickerToOpen(false)}
+                    anchorEl={dateTimePickerToAnchorRef}
+                    selectedDate={searchFormInput.pickUpTimeTo || null}
+                    onDateChange={(date) => {
+                      setSearchFormInput({ ...searchFormInput, pickUpTimeTo: date });
+                      setIsSearchSubmitted(false);
+                    }}
+                    locale={enLocale}
+                  />
+                  <CustomizedButton title="Search" type="warning" click={() => { }} />
+                </Box>
+                {isSearchSubmitted &&
+                  (searchFormInput.pickUpTimeFrom && searchFormInput.pickUpTimeTo ? (
+                    <p className="font-aleo text-gray-700 mb-4 text-sm bg-blue-50 border-l-4 border-blue-400 py-2 px-4 rounded w-fit">
+                      Showing Bookings from{" "}
+                      <strong>
+                        {searchFormInput.pickUpTimeFrom.toISOString().split("T")[0]}
+                      </strong>{" "}
+                      to{" "}
+                      <strong>
+                        {searchFormInput.pickUpTimeTo.toISOString().split("T")[0]}
+                      </strong>
+                    </p>
+                  ) : searchFormInput.pickUpTimeFrom ? (
+                    <p className="font-aleo text-gray-700 mb-4 text-sm bg-blue-50 border-l-4 border-blue-400 py-2 px-4 rounded w-fit">
+                      Showing Bookings from{" "}
+                      <strong>
+                        {searchFormInput.pickUpTimeFrom.toISOString().split("T")[0]}
+                      </strong>{" "}
+                      to <strong>{new Date().toISOString().split("T")[0]}</strong>
+                    </p>
+                  ) : searchFormInput.pickUpTimeTo ? (
+                    <p className="font-aleo text-gray-700 mb-4 text-sm bg-blue-50 border-l-4 border-blue-400 py-2 px-4 rounded w-fit">
+                      Showing Bookings up to{" "}
+                      <strong>
+                        {searchFormInput.pickUpTimeTo.toISOString().split("T")[0]}
+                      </strong>
+                    </p>
+                  ) : null)}
+
+                {isLoading ? (
+                  <Typography
+                    sx={{ color: "gray", fontSize: 16, textAlign: "center", my: 10 }}
+                  >
+                    Getting your bookings...
+                  </Typography>
+                ) : bookingListData.length === 0 ? (
+                  <Typography
+                    sx={{ color: "gray", fontSize: 16, textAlign: "center", my: 10 }}
+                  >
+                    No bookings to show.
+                  </Typography>
+                ) : (
+                  <TableContainer
+                    component={Paper}
+                    sx={{ boxShadow: "none", border: "none" }}
+                  >
+                    <Table
+                      sx={{ minWidth: 500, borderCollapse: "collapse" }}
+                      aria-label="custom pagination table"
+                    >
+                      <TableHead>
+                        <TableRow>
+                          <StyledTableCell>Pick-up Time</StyledTableCell>
+                          <StyledTableCell>From</StyledTableCell>
+                          <StyledTableCell>To</StyledTableCell>
+                          <StyledTableCell>Booking Status</StyledTableCell>
+                          <StyledTableCell>Operation</StyledTableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {bookingListData &&
+                          bookingListData.map((row, index) => (
+                            <TableRow
+                              key={index}
+                              sx={{
+                                "&:hover": { bgcolor: "#f9fafb" },
+                                transition: "background-color 0.2s",
+                              }}
+                            >
+                              <StyledTableCell>
+                                {row.trip.pickup_time
+                                  ? new Date(row.trip.pickup_time).toLocaleString()
+                                  : "N/A"}
+                              </StyledTableCell>
+                              <StyledTableCell>
+                                {row.trip.airport === "" || row.trip.airport === null
+                                  ? row.trip.pickup_location
+                                  : row.trip.airport}
+                              </StyledTableCell>
+                              <StyledTableCell>
+                                {row.trip.dropoff_location}
+                              </StyledTableCell>
+                              <StyledTableCell>
+                                <span
+                                  className={`inline-block px-5 py-1 rounded-full text-xs font-medium ${row.booking_status === "Approved"
+                                    ? "bg-green-100 text-green-800 border border-green-800"
+                                    : row.booking_status === "Rejected"
+                                      ? "bg-red-100 text-red-800 border border-red-800"
+                                      : row.booking_status === "Cancelled"
+                                        ? "bg-gray-300 text-gray-900 border border-gray-900"
+                                        : "bg-yellow-100 text-yellow-800 border border-yellow-800"
+                                    }`}
+                                >
+                                  {row.booking_status}
+                                </span>
+                              </StyledTableCell>
+                              <StyledTableCell>
+                                <div className="flex gap-2 justify-center">
+                                  <CustomizedButton
+                                    click={() => handleViewDialogOpen(row)}
+                                    type="warning"
+                                    title="View"
+                                  />
+                                  {row.booking_status === "Pending" && (
+                                    <CustomizedButton
+                                      click={() => handleEditDialogOpen(row)}
+                                      type="warning"
+                                      title="Edit"
+                                    />
+                                  )}
+                                  {row.booking_status === "Pending" && (
+                                    <CustomizedButton
+                                      click={() => handleCancelBooking(row)}
+                                      type="error"
+                                      title="Cancel"
+                                    />
+                                  )}
+                                </div>
+                              </StyledTableCell>
+                            </TableRow>
+                          ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                )}
+                <ViewDialog
+                  viewData={bookDetail!}
+                  dialogOpen={bookDetailDialogOpen}
+                  handleDialogClose={handleViewDialogClose}
+                />
+                <Dialog
+                  onClose={handleEditDialogClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={editBookDialogOpen}
+                  maxWidth="md"
+                >
+                  <DialogTitle
+                    sx={{
+                      m: 0,
+                      p: 2,
+                      fontFamily: "inter",
+                      fontWeight: "bold",
+                      bgcolor: "#2c2c2c",
+                      color: "white",
+                      textAlign: "center",
+                      fontSize: 28,
+                    }}
+                    id="customized-dialog-title"
+                  >
+                    Edit Booking
+                  </DialogTitle>
+                  <IconButton
+                    aria-label="close"
+                    onClick={handleEditDialogClose}
+                    sx={(theme) => ({
+                      position: "absolute",
+                      right: 8,
+                      top: 8,
+                      color: theme.palette.grey[500],
+                    })}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <DialogContent dividers>
+                    <BookingPage />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button
+                      sx={{
+                        color: "#2c2c2c",
+                        transition: "all 300ms",
+                        mr: 1,
+                        ":hover": { bgcolor: "#2c2c2c", color: "white" },
+                      }}
+                      onClick={handleCancelDialogClose}
+                    >
+                      Close
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+                <ConfirmDialog
+                  open={cancelBookDialogOpen}
+                  dialogTitle="Cancel Booking"
+                  confirmMessage="Are you sure you want to cancel this booking?"
+                  confirmCallBack={handleConfirmCancel}
+                  cancelCallBack={handleCancelDialogClose}
+                />
+                <div className="flex justify-center mt-4">
+                  <TablePagination
+                    component="div"
+                    rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                    count={bookingListCount}
+                    rowsPerPage={paginationMeta.pageSize}
+                    page={paginationMeta.page}
+                    slotProps={{
+                      select: {
+                        inputProps: {
+                          "aria-label": "rows per page",
+                        },
+                        native: true,
+                      },
+                    }}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangePageSize}
+                    ActionsComponent={TablePaginationActions}
+                  />
+                </div>
+              </motion.div>
           </div>
         )}
       </div>  
