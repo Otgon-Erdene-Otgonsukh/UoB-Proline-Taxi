ce4c9510877cd759be983de758f0a1f20c576410
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-03-05 17:05:57 +0800

    add department manage page in a separate folder

---
+++ uob_transport_app/app/super/departmentManagePage.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Department Management</h1>
+    </div>
+  )
+}
+
+export default page;
--- uob_transport_app/app/super/page.tsx
+++ uob_transport_app/app/super/page.tsx
@@ -42,6 +42,7 @@ import EditDialog from "./userManageComponents/eidtDialog";
 import { userStatusToIntMap, userStatusToStrMap, roleStrMap, roles, roleReadableStrMap } from "./constants";
 import ConfirmDialog from "@/components/confirmDIalog";
 import { UserTable } from "@/components/SuperUsersTable";
+import DepartmentManagePage from "./departmentManagePage";
 
 interface TablePaginationActionsProps {
   count: number;
@@ -271,7 +272,7 @@ const Page = () => {
 
   };
 
-  const [tabValue, setTabValue] = useState(0);
+  const [tabValue, setTabValue] = useState(1);
 
   return (
     <div className="flex flex-col min-h-screen items-center pt-15 p-4">
@@ -450,9 +451,7 @@ const Page = () => {
                 Departments
               </h1>
             </div>
-            <Typography sx={{ color: "gray", fontSize: 16, textAlign: "center", my: 10 }}>
-              Departments Table View (to be implemented)
-            </Typography>
+            <DepartmentManagePage />
           </div>
         )}
       </motion.div>
