6b6aa26deecc3631d909e7b9ca1a6a4baa5994ef
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-03-05 18:15:48 +0800

    add search form

--- uob_transport_app/app/super/departmentManagePage.tsx
+++ uob_transport_app/app/super/departmentManagePage.tsx
@@ -3,7 +3,8 @@
 import {
   Table, TableBody, TableHead, TableRow, TableContainer, Paper,
   Box,
-  Button
+  Button,
+  TextField
 } from "@mui/material";
 import { useState, useEffect } from "react";
 import { DepartmentRecord } from "@/model/models";
@@ -38,6 +39,8 @@ const DepartmentManagePage = () => {
       }
     ]);
   }, []);
+  
+  const [searchFormInput, setSearchFormInput] = useState({name: ""})
 
   const handleViewManager = (manager: DepartmentRecord["manager"]) => {
     console.log(manager);
@@ -50,46 +53,92 @@ const DepartmentManagePage = () => {
   const handleEdit = (department: DepartmentRecord) => {
     console.log(department);
   };
-  
+
   const handleDelete = (department: DepartmentRecord) => {
     console.log(department)
   };
 
+  const handleSubmitSearchForm = () => {
+    console.log(searchFormInput);
+  }
+
   return (
-    <Box>
-      <TableContainer component={Paper} sx={{ boxShadow: "none", border: "none" }}>
-        <Table sx={{ minWidth: 500, borderCollapse: "collapse" }} aria-label="department table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Name</StyledTableCell>
-              <StyledTableCell>Manager</StyledTableCell>
-              <StyledTableCell>Member Count</StyledTableCell>
-              <StyledTableCell>Operation</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {departments.map((department) => (
-              <TableRow key={department.dep_id}>
-                <StyledTableCell>{department.dep_name}</StyledTableCell>
-                <StyledTableCell>
-                  <Button onClick={() => handleViewManager(department.manager)}>
-                    {department.manager.full_name}
-                  </Button>
-                </StyledTableCell>
-                <StyledTableCell>{department.member_count}</StyledTableCell>
-                <StyledTableCell>
-                  <div className="flex gap-2 justify-center">
-                    <CustomizedButton type="primary" click={() => handleView(department)} title="View" />
-                    <CustomizedButton type="warning" click={() => handleEdit(department)} title="Edit" />
-                    <CustomizedButton type="error" click={() => handleDelete(department)} title="Delete" />
-                  </div>
-                </StyledTableCell>
+    <>
+      <div className="flex justify-between items-center mb-4">
+        <Box
+          component="form"
+          onSubmit={handleSubmitSearchForm}
+          sx={{
+            display: "flex",
+            gap: 2.5,
+          }}
+        >
+          <TextField
+            fullWidth
+            label="Name"
+            id="searchNameInput"
+            value={searchFormInput.name}
+            onChange={(e) => { setSearchFormInput({ ...searchFormInput, name: e.target.value }); }}
+            size="small"
+            sx={{ minWidth: 150 }}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            sx={{
+              bgcolor: "#2c2c2c",
+              color: "white",
+              borderRadius: "0.375rem",
+              fontSize: "0.875rem",
+              fontWeight: 300,
+              "&:hover": {
+                bgcolor: "#414040",
+                transform: "scale(1.01)",
+              },
+              transition: "all 0.2s",
+            }}
+            size="small"
+          >
+            Search
+          </Button>
+        </Box>
+      </div>
+      <Box>
+        <TableContainer component={Paper} sx={{ boxShadow: "none", border: "none" }}>
+          <Table sx={{ minWidth: 500, borderCollapse: "collapse" }} aria-label="department table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Name</StyledTableCell>
+                <StyledTableCell>Manager</StyledTableCell>
+                <StyledTableCell>Member Count</StyledTableCell>
+                <StyledTableCell>Operation</StyledTableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+            </TableHead>
+            <TableBody>
+              {departments.map((department) => (
+                <TableRow key={department.dep_id}>
+                  <StyledTableCell>{department.dep_name}</StyledTableCell>
+                  <StyledTableCell>
+                    <Button onClick={() => handleViewManager(department.manager)}>
+                      {department.manager.full_name}
+                    </Button>
+                  </StyledTableCell>
+                  <StyledTableCell>{department.member_count}</StyledTableCell>
+                  <StyledTableCell>
+                    <div className="flex gap-2 justify-center">
+                      <CustomizedButton type="primary" click={() => handleView(department)} title="View" />
+                      <CustomizedButton type="warning" click={() => handleEdit(department)} title="Edit" />
+                      <CustomizedButton type="error" click={() => handleDelete(department)} title="Delete" />
+                    </div>
+                  </StyledTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
   );
 };
 
