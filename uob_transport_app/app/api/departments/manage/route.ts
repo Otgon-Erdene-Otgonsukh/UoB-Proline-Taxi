f55323753d4740cc8a9916d0730810d14715932b
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-03-07 13:30:10 +0800

    add new api

---
+++ uob_transport_app/app/api/departments/manage/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from "next/server";
+import { getDepartmentListIncludeManagerIdAccess } from "@/backend/access/departments_access";
+
+export async function GET(request: NextRequest) {
+  // TODO Check super admin
+
+  const searchParams = request.nextUrl.searchParams;
+  const depName = searchParams.get('depName') || undefined
+
+  const departmentsList = await getDepartmentListIncludeManagerIdAccess(depName)
+
+  return new Response(JSON.stringify(departmentsList), { status: 200 });
+
+}
