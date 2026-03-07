b0244f8cd32277e8d12456194e1e8bc256a898d9
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-03-07 14:09:38 +0800

    modify backend api

--- uob_transport_app/app/api/departments/manage/route.ts
+++ uob_transport_app/app/api/departments/manage/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
 import { getDepartmentListIncludeManagerIdAccess } from "@/backend/access/departments_access";
+import { getUsersByIdsAccess } from "@/backend/access/user_access";
+import { User } from "@/generated/prisma/client";
 
 export async function GET(request: NextRequest) {
   // TODO Check super admin
@@ -8,7 +10,22 @@ export async function GET(request: NextRequest) {
   const depName = searchParams.get('depName') || undefined
 
   const departmentsList = await getDepartmentListIncludeManagerIdAccess(depName)
+  const managerIds = departmentsList.map(e => {
+    return e.manager_id
+  }).filter(e => {
+    return e !== null
+  })
+  const managers = await getUsersByIdsAccess(managerIds)
+  const managerIdMap: {[key: number]: User} = {}
+  managers.forEach(e => {
+    managerIdMap[e.user_id] = e
+  })
 
-  return new Response(JSON.stringify(departmentsList), { status: 200 });
+  return new Response(JSON.stringify(departmentsList.map(e => {
+    return {
+      ...e,
+      manager: e.manager_id ? managerIdMap[e.manager_id] : null
+    }
+  })), { status: 200 });
 
 }
