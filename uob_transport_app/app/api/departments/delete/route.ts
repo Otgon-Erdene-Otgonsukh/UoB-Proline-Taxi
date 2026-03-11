ba798cd8167bc510ae6e5c626643e2768fdbd101
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-03-11 20:18:25 +0800

    modify delete department api to check the user count

--- uob_transport_app/app/api/departments/delete/route.ts
+++ uob_transport_app/app/api/departments/delete/route.ts
@@ -1,10 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createNewDepartmentAccess, deleteDepartmentAccess } from "@/backend/access/departments_access";
+import { createNewDepartmentAccess, deleteDepartmentAccess, getDepartmentByIdAccess } from "@/backend/access/departments_access";
 
 export async function POST(req: NextRequest) {
   // TODO check super admin
   const request = await req.json();
   const id: number = request.depId;
+
+  getDepartmentByIdAccess(id).then((department) => {
+    if (department) {
+      if (department._count.User > 0) {
+        return NextResponse.json({
+          status: 201,
+          message: "Department has users, cannot be deleted!"
+        }, { status: 201 });
+      }
+    } else {
+      return NextResponse.json({
+        status: 201,
+        message: "Department not found!"
+      }, { status: 201 });
+    }
+  })
 
   const department = await deleteDepartmentAccess(id);
 
