e76232a69744a3d864d83976398489ee5a69ddb1
Erik Maltby <maltby.e@outlook.com>
2026-03-18 12:47:37 +0000

    missing requests for the super booking page

--- uob_transport_app/app/super/request.ts
+++ uob_transport_app/app/super/request.ts
@@ -60,3 +60,17 @@ export const changeDepartmentForUsers = async (userIds: number[], depId: number)
     depId
   })
 }
+
+export const getBookingList = async (page: number, pageSize: number, searchParams: { from?: string, to?: string, bookingStatus?: string, pickUpTimeFrom?: string, pickUpTimeTo?: string }): Promise<Response> => {
+  return easyGetRequest('booking-list', {
+    page,
+    pageSize,
+    ...searchParams
+  })
+}
+
+export const cancelBooking = async (bookingId: number): Promise<Response> => {
+  return easyPostRequest('cancel-booking', {
+    bookingId
+  })
+}
