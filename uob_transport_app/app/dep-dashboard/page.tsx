088de83abb5d3252149fc825c5e602f394be61d4
Ioan Moir <ok24616@bristol.ac.uk>
2026-03-27 15:20:33 +0000

    Switch dep dashboard to use approve_booking

--- uob_transport_app/app/dep-dashboard/page.tsx
+++ uob_transport_app/app/dep-dashboard/page.tsx
@@ -244,7 +244,7 @@ export default function DepDashboard() {
               : b,
           ),
         );
-        fetch("api/update_booking", {
+        fetch("api/approve_booking", {
           method: "POST",
           body: JSON.stringify({
             bookingId: pendingBookingId,
@@ -271,7 +271,7 @@ export default function DepDashboard() {
         b.booking_id === bookingId ? { ...b, booking_status: "Rejected" } : b,
       ),
     );
-    fetch("api/update_booking", {
+    fetch("api/approve_booking", {
       method: "POST",
       body: JSON.stringify({ bookingId: bookingId, newStatus: "Rejected" }),
     });
