1c584a2445ebe7e3e16e35fbd795c19f13e0e1f9
Yidi <152907002+Dehors777@users.noreply.github.com>
2026-04-05 21:50:42 +0800

    add new test for update_booking_status

--- uob_transport_app/__test__/backend_functions/update_booking_status.test.ts
+++ uob_transport_app/__test__/backend_functions/update_booking_status.test.ts
@@ -152,4 +152,32 @@ describe("Update booking status backend test", () => {
     expect(result).toBeInstanceOf(Error);
   });
 
+  test("returns error when user not found", async () => {
+    prismaMock.booking.update.mockResolvedValue({});
+    prismaMock.booking.findUnique.mockResolvedValue({
+      user_id: 1,
+      email: "a",
+      passenger_name: "x",
+      tel_number: "1",
+      department: { dep_name: "CS" },
+      trip: {
+        pickup_location: JSON.stringify({}),
+        dropoff_location: JSON.stringify({}),
+        via: null,
+        airport: null,
+        return_drop_loc: null,
+        flight_num: null,
+        pickup_time: new Date(),
+        return_pickup_time: null,
+        price: 1,
+      },
+    });
+
+    prismaMock.user.findUnique.mockResolvedValue(null);
+    prismaMock.user.findFirst.mockResolvedValue({ email: "admin@test.com" });
+
+    const result = await updateBookingStatus(1, "Approved", "PO");
+
+    expect(result).toBeInstanceOf(Error);
+  });
 });
