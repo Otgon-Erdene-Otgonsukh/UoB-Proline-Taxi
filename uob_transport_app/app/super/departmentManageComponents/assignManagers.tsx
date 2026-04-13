f9f2152672742eb542b608e2320ceae972f00fc7
Erik Maltby <maltby.e@outlook.com>
2026-04-13 12:05:44 +0100

    showing staff assigned to the department and allows selection of one entry

--- uob_transport_app/app/super/departmentManageComponents/assignManagers.tsx
+++ uob_transport_app/app/super/departmentManageComponents/assignManagers.tsx
@@ -8,20 +8,57 @@ import {
     Button,
     Typography,
     IconButton,
+    Box,
+    TableContainer,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableBody,
+    Checkbox,
 } from "@mui/material";
 import {
     Close as CloseIcon,
 } from "@mui/icons-material"
+import PersonOffIcon from '@mui/icons-material/PersonOff';
 import { DepartmentRecord } from "@/model/models";
 import { roleReadableStrMap, userStatusToIntMap, userStatusToStrMap } from "../constants";
+import { useEffect, useState } from "react";
+import { getUsersByDepId } from "../request";
+import { StyledStickyTableCell } from "@/components/StyledTableCell";
 
 const AssignManagerDialog = ({ viewData, dialogOpen, handleDialogClose }: { viewData: DepartmentRecord, dialogOpen: boolean, handleDialogClose: () => void }) => {
+
+    const [members, setMembers] = useState<{ user_id: number, full_name: string, email: string, phone_number: string, role: string }[]>([]);
+    const [isLoading, setIsLoading] = useState(true)
+    const [departmentData, setDepartmentData] = useState<DepartmentRecord>(viewData);
+    const [selected, setSelected] = useState<number | null>(null);
+
+    useEffect(() => {
+        if (!viewData?.depId) return;
+
+        setIsLoading(true);
+        getUsersByDepId(viewData.depId).then(async (res) => {
+            if (res.status === 200) {
+                const data = await res.json()
+                console.log(data);
+                setMembers(data)
+            }
+        }).finally(() => {
+            setIsLoading(false);
+        });
+    }, [viewData])
+
+    const handleClickRow = (userId: number) => {
+        setSelected(selected === userId ? null : userId);
+    };
 
     return (<div>
         <Dialog
             onClose={handleDialogClose}
             aria-labelledby="customized-dialog-title"
             open={dialogOpen}
+            maxWidth="md"
             sx={{
                 "& .MuiStack-root": {
                     flexDirection: "row",
@@ -46,9 +83,100 @@ const AssignManagerDialog = ({ viewData, dialogOpen, handleDialogClose }: { view
             >
                 Assign Managers
             </DialogTitle>
-            <Typography sx={{ p: 3 }}>
-                department: {viewData?.depName}
-            </Typography>
+            <IconButton
+                aria-label="close"
+                onClick={handleDialogClose}
+                sx={(theme) => ({
+                    position: "absolute",
+                    right: 8,
+                    top: 8,
+                    color: theme.palette.grey[500],
+                })}
+            >
+                <CloseIcon />
+            </IconButton>
+            <DialogContent dividers>
+                <div style={{ width: "100%", display: "flex", justifyContent: "space-between" }}>
+                    <Typography gutterBottom sx={{ fontWeight: "bold", fontSize: 20 }}>
+                        Department Name:
+                    </Typography>
+                    <Typography gutterBottom sx={{ fontSize: 20 }}>
+                        {viewData?.depName}
+                    </Typography>
+                </div>
+                <Typography gutterBottom sx={{ fontWeight: "bold", fontSize: 18, mt: 2 }}>
+                    Members (Total {members.length}):
+                </Typography>
+                <Box sx={{ marginTop: 2 }}>
+                    {isLoading ? <Typography sx={{ textAlign: "center", color: "gray" }}>Loading members...</Typography> : members.length === 0 ? <Typography sx={{ textAlign: "center", color: "gray" }}><PersonOffIcon sx={{ mb: 0.5 }} /> No users to show.</Typography> :
+                        (<TableContainer component={Paper} sx={{ boxShadow: "none", border: "none", maxHeight: 600 }}>
+                            <Table stickyHeader sx={{ minWidth: 500 }} aria-label="department members table" size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <StyledStickyTableCell padding="checkbox">
+                                            Select
+                                        </StyledStickyTableCell>
+                                        <StyledStickyTableCell>Name</StyledStickyTableCell>
+                                        <StyledStickyTableCell>Email</StyledStickyTableCell>
+                                        <StyledStickyTableCell>Phone number</StyledStickyTableCell>
+                                        <StyledStickyTableCell>Role</StyledStickyTableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {members.map((member) => {
+                                        const isItemSelected = selected === member.user_id;
+                                        return (
+                                            <TableRow
+                                                hover
+                                                onClick={() => handleClickRow(member.user_id)}
+                                                role="checkbox"
+                                                aria-checked={isItemSelected}
+                                                tabIndex={-1}
+                                                key={member.user_id}
+                                                selected={isItemSelected}
+                                                sx={{ cursor: 'pointer' }}
+                                            >
+                                                <StyledStickyTableCell padding="checkbox">
+                                                    <Checkbox
+                                                        color="primary"
+                                                        checked={isItemSelected}
+                                                    />
+                                                </StyledStickyTableCell>
+                                                <StyledStickyTableCell>{member.full_name}</StyledStickyTableCell>
+                                                <StyledStickyTableCell>{member.email}</StyledStickyTableCell>
+                                                <StyledStickyTableCell>{member.phone_number}</StyledStickyTableCell>
+                                                <StyledStickyTableCell>{roleReadableStrMap[member.role]}</StyledStickyTableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>)
+                    }
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                
+                <Button>
+                    Assign as Manager
+                </Button>
+
+                <Button>
+                    Assign as Finance Manager
+                </Button>
+
+                <Button
+                    sx={{
+                        color: "#2c2c2c",
+                        mr: 1,
+                        transition: "all 250ms",
+                        ":hover": { bgcolor: "#2c2c2c", color: "white" },
+                    }}
+                    onClick={handleDialogClose}
+                >
+                    Close
+                </Button>
+            </DialogActions>
         </Dialog>
     </div>)
 }
